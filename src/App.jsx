11ad1bd368b2b492751735e85da53d2f190d4811
itskheila <munenetiara@gmail.com>
2025-07-29 23:10:42 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import react from "react";
+import {Routes, Route} from "react-router-dom";
+
+function App(){
+    return (
+        <div>
+            <h1>Memory Card Game</h1>
+            <Routes>
+                
+            </Routes>
+           
+        </div>
+    );
+}
+
+export default App;
