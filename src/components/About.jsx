cda3073f4b4c42d3005e960a3c33b7cf8495e4b7
itskheila <munenetiara@gmail.com>
2025-07-30 21:03:59 +0300

    about page commit

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+function About (){
+return(
+    <div>
+        <h2>About the Memory Card Game</h2>
+        <p>This is a simple memory card game where players match pairs of fruit cards.</p>
+        <p>Click on a card to flip it and reveal the fruit. Try to find all matching pairs!</p>
+        <p>Enjoy the game!</p>
+    </div>
+)
+
+}
+
+export default About;
